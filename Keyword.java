dc635394f5838907852ffa24e7dc5ed1668b2073
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-01-26 20:00:33 +0000

    Implemented Keyword class, which will store the keyword, priority, and decomposistions.

---
+++ Keyword.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class Keyword {
+    private final String word;
+    private int priority;
+    private ArrayList<Decomposition> decomposition;
+
+    public Keyword(String word) {
+        this.word = word;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public void setDecomposition(ArrayList<Decomposition> decomposition) {
+        this.decomposition = decomposition;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int getPriority() { return priority; }
+
+    public ArrayList<Decomposition> getDecomposition() { return 
+decomposition; }
+
+    public String toString() {
+        return this.word + " " + this.getPriority() + "\n" + 
+decomposition.toString();
+    }
+}
