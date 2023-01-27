9a593070766844303175a2b258c6626d0deaf733
Mia Cousins <mc431@st-andrews.ac.uk>
2023-01-27 10:34:23 +0000

    Creating start, quit and  basic message rules in the engine

--- Engine.java
+++ Engine.java
@@ -1,2 +1,74 @@
-import java.io.FileNotFoundException
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Engine {
+    private final ArrayList<String> welcomeMessages = new ArrayList<String>();
+    private final ArrayList<String> quit = new ArrayList<String>();
+    private final ArrayList<String> closingMessages = new ArrayList<String>();
+    private final ArrayList<Substitution> preSubs = new ArrayList<Substitution>();
+    private final ArrayList<Substitution> postSubs = new ArrayList<Substitution>();
+    private ArrayList<Keyword> keywords = new ArrayList<Keyword>();
+    private boolean programAlive = true;
+
+    public void run(String file) throws FileNotFoundException {
+        try {
+            Scanner scan = new Scanner(new FileReader(file));
+            scan.nextLine();
+            startingMessages(scan);
+            goodbyeMessages(scan);
+            quitMessages(scan);
+            preSubstitutionRules(scan);
+            postSubstitutionRules(scan);
+            generateKeywords(scan);
+            sortKeywords();
+        } catch (FileNotFoundException e) {
+            System.out.println("Script file not found: " + e);
+        }
+    }
+
+    private void startingMessages(Scanner scan) {
+        String line = scan.nextLine();
+        while (line.startsWith("start: ")) {
+            int index = line.indexOf(":");
+            this.welcomeMessages.add(line.substring(index + 2));
+            line = scan.nextLine();
+        }
+    }
+    private void goodbyeMessages(Scanner scan) {
+        String line = scan.nextLine();
+        while (line.startsWith("end: ")) {
+            int index = line.indexOf(":");
+            this.closingMessages.add(line.substring(index + 2));
+            line = scan.nextLine();
+        }
+    }
+    private void quitMessages(Scanner scan) {
+        String line = scan.nextLine();
+        while (line.contains("quit")) {
+            int index = line.indexOf(":");
+            this.quit.add(line.substring(index + 2));
+            line = scan.nextLine();
+        }
+    }
+    private void preSubstitutionRules(Scanner scan) {
+        String line = scan.nextLine();
+        while (line.contains("pre")) {
+            String[] words = line.split(" ");
+            this.preSubs.add(new Substitution(words[1], words[2]));
+            line = scan.nextLine();
+        }
+    }
+
+    public void postSubstitutionRules(Scanner scan) {
+        String line = scan.nextLine();
+        while (line.contains("post")) {
+            String[] words = line.split(" ");
+            this.postSubs.add(new Substitution(words[1], words[2]));
+            line = scan.nextLine();
+        }
+    }
+}
 
