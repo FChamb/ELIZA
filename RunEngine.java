552d6a53d33ad8afe9dcd25975bce4097622b06c
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-01-28 20:20:41 +0000

    Implemented RunEngine class -- not fully functional, work in progress

---
+++ RunEngine.java
@@ -0,0 +1,38 @@
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class RunEngine {
+    private final String eliza = "Eliza: ";
+    private final String user = ">>";
+
+    public static void main(String[] args) {
+        try {
+            Engine engine = new Engine();
+            engine.run(args[0]);
+            RunEngine run = new RunEngine();
+            run.beginEliza(engine);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Please provide a script for the engine: " + e);
+        } catch (FileNotFoundException e) {
+            System.out.println("That script file can not be found: " + e);
+        }
+    }
+
+    public void beginEliza(Engine engine) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print(eliza);
+        engine.getWelcomeMesssages();
+        while (engine.getProgramALive()) {
+            System.out.print(user);
+            String line = scan.nextLine().toLowerCase();
+            String[] words = line.split(" ");
+            engine.checkQuitMessages(line);
+            System.out.print(eliza);
+            System.out.println(engine.generateResponse(line, words));
+        }
+        System.out.print(eliza);
+        engine.getClosingMessages();
+    }
+}
+
