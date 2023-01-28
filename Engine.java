22c06ecf1f3c1284f12822388d38d56ddfba9988
Finnegan Z. Chamberlain <fc84@st-andrews.ac.uk>
2023-01-28 14:01:48 +0000

    Continued work on Engine. Added generate response method -- Still needs major work. Basic communication working

--- Engine.java
+++ Engine.java
@@ -142,6 +142,141 @@ public class Engine {
         System.out.println(closingMessages.get(randomElement(closingMessages)));
     }
 
+
+    public boolean checkQuitMessages(String line) {
+        for (int i = 0; i < quit.size(); i++) {
+            if (line.contains(quit.get(i))) {
+                this.programAlive = false;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String generateResponse(String line, String[] words) {
+        ArrayList<Keyword> possibleKeyword = new ArrayList<Keyword>();
+        ArrayList<String> responses = new ArrayList<String>();
+        String[] responseWords;
+        String takenFromLine = "";
+        String response = "";
+        String preSubLine = correctPreSubs(words);
+        for (String word : words) {
+            ArrayList<Keyword> key = isWordKeyword(word);
+            if (key.size() != 0) {
+                possibleKeyword.addAll(key);
+            }
+        }
+        Keyword highest = getHighestPriority(possibleKeyword);
+        responses = checkDecomposition(preSubLine, highest);
+        responses.addAll(checkReferenceTo(responses));
+        response = responses.get(randomElement(responses));
+        responseWords = response.split(" ");
+        for (String word : responseWords) {
+            word = word.replaceAll("[.,?!]", "");
+            if (word.contains("(r)")) {
+                int index = line.indexOf(highest.getWord());
+                //System.out.println(index);
+                //System.out.println(line);
+                //System.out.println(response);
+                takenFromLine = line.substring(index);
+                takenFromLine = correctPostSubs(takenFromLine);
+                response = response.replace("(r)", takenFromLine);
+                return response;
+            }
+        }
+        return response;
+    }
+
+    public ArrayList<Keyword> isWordKeyword(String word) {
+        ArrayList<Keyword> possibleKeywords = new ArrayList<Keyword>();
+        for (Keyword key : keywords) {
+            if (key.getWord().equals(word) || key.getWord().equals("NONE")) {
+                possibleKeywords.add(key);
+            }
+        }
+        return possibleKeywords;
+    }
+
+    public ArrayList<String> checkDecomposition(String line, Keyword keyword) {
+        for (Decomposition decomp : keyword.getDecomposition()) {
+            if (line.contains(decomp.getDecomposition())) {
+                return decomp.getReassembly();
+            }
+        }
+        return keyword.getDecomposition().get(0).getReassembly();
+    }
+
+    public ArrayList<String> checkReferenceTo(ArrayList<String> responses) {
+        for (String resp : responses) {
+            if (resp.contains("referto")) {
+                String newResp = resp.substring(resp.indexOf(" ") + 1);
+                for (Keyword key : keywords) {
+                    if (key.getWord().equals(newResp)) {
+                        responses.remove(resp);
+                        responses.add(key.getDecomposition().get(0).getReassembly().get(randomElement(key.getDecomposition().get(0).getReassembly())));
+                    }
+                }
+            }
+        }
+        return responses;
+    }
+
+    public Keyword getHighestPriority(ArrayList<Keyword> keywords) {
+        int highest = 0;
+        Keyword highestKey = null;
+        for (Keyword key : keywords) {
+            if (key.getPriority() >= highest) {
+                highest = key.getPriority();
+                highestKey = key;
+            }
+        }
+        return highestKey;
+    }
+
+    public String correctPreSubs(String[] words) {
+        String[] newWords = new String[words.length];
+        for (int i = 0; i < newWords.length; i++) {
+            newWords[i] = words[i];
+        }
+        String finalLine = "";
+        for (int i = 0; i < words.length; i++) {
+            for (Substitution sub : preSubs) {
+                if (words[i].contains(sub.getBefore())) {
+                    newWords[i] = sub.getAfter();
+                    break;
+                } else {
+                    newWords[i] = words[i];
+                }
+            }
+        }
+        for (String word : newWords) {
+            finalLine += (word + " ");
+        }
+        return finalLine;
+    }
+
+    public String correctPostSubs(String line) {
+        String[] words = line.split(" ");
+        String finalLine = "";
+        for (int i = 0; i < words.length; i++) {
+            for (Substitution sub : postSubs) {
+                if (words[i].contains(sub.getBefore())) {
+                    words[i] = sub.getAfter();
+                    break;
+                }
+            }
+        }
+        for (String word : words) {
+            finalLine += (word + " ");
+        }
+        System.out.println(finalLine);
+        return finalLine;
+    }
+
+    public boolean getProgramALive() {
+        return this.programAlive;
+    }
+
     public int randomElement(ArrayList list) {
         return (int) (Math.random() * list.size());
     }
